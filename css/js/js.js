bb32059907492790241e0105fd60efcbdd44172f
gyaninascimento <gyanipnascimento@gmail.com>
2021-09-13 11:46:17 -0300

    Create JS
    Aula 1 Alura

---
+++ css/js/js.js
@@ -0,0 +1,19 @@
+var nome = "Gyani"//variáveis: armazena informações no computador
+
+var notaDoPrimeiroBimestre = 9.45
+var notaDoSegundoBimestre = 7.58
+var notaDoTerceiroBimestre = 4.65
+var notaDoQuartoBimestre = 8.18
+
+var notaFinal = (notaDoPrimeiroBimestre
++ notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre)/4
+
+var notaFixada = notaFinal.toFixed (2)//fixar casas decimais, arrendoda a nota. toFixed é uma função
+
+console.log("Bem vinda " + nome)
+console.log(notaDoPrimeiroBimestre)
+console.log(notaFixada)
+//revisão
+// o que são variaveis: são espaços na memoria do computador que utilizamos para guardar/referenciar um valor
+//função toFixed: para fixar valores
+//estilizar a pagina, se foi aprovado ou nao quebrar em 2 linhas , fazer um alert escrever na pagina em js, fazer conversor de temperatura, conta inteira no console.log9
